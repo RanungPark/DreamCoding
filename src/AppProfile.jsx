4d3f1c6da24eabc8b86b5c5a7facc5ab95d8a21b
RanungPark <ckehfqkr302@naver.com>
2023-05-18 21:28:43 +0900

    🚧 add Event button

--- src/AppProfile.jsx
+++ src/AppProfile.jsx
@@ -3,8 +3,13 @@ import Avartar from './components/Avartar';
 import Profile from './components/Profile';
 
 function AppProfile() {
+  const handleClick = (event)=>{
+    console.log(event);
+    alert('버튼이 클릭됨!')
+  }
   return (
     <>
+    <button onClick={handleClick} type='button'>버튼</button>
       <Avartar 
       image='https://images.unsplash.com/photo-1527980965255-d3b416303d12?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1180&q=80'
       isNew={true}
