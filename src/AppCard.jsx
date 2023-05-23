69cad113e8f8478b278c6d1234fd9e560e1d7e1d
RanungPark <ckehfqkr302@naver.com>
2023-05-23 17:31:23 +0900

    🚧 Wrap ex)

---
+++ src/AppCard.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+
+export default function AppCard() {
+  return (
+    <div>
+      <Card>
+        <p>Card1</p>
+      </Card>
+
+      <Card>
+        <h1>Card2</h1>
+        <p>실행</p>
+      </Card>
+    </div>
+  );
+}
+
+
+function Card({children}) {
+  return (
+    <div
+    style={{
+      backgroundColor: 'black',
+      borderRadius: '20px',
+      color: 'white',
+      minHeight: '200px',
+      maxWidth: '200px',
+      margin: '1rem',
+      padding: '1rem',
+      textAlign: 'center',
+    }}
+    >
+      {children}
+    </div>
+  )
+}
